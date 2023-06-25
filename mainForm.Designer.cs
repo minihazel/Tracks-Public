c7e0ccb1f57f3fb41263e026f32e74e3f81bd082
hazelify <decyanidez@gmail.com>
2023-06-25 02:05:52 +0200

    Add project files.

---
+++ mainForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace LayoutCustomization
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b_placeholder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b_placeholder
+            // 
+            this.b_placeholder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.b_placeholder.AutoSize = true;
+            this.b_placeholder.Font = new System.Drawing.Font("Bahnschrift Light", 5F);
+            this.b_placeholder.Location = new System.Drawing.Point(12, 584);
+            this.b_placeholder.Name = "b_placeholder";
+            this.b_placeholder.Size = new System.Drawing.Size(42, 8);
+            this.b_placeholder.TabIndex = 0;
+            this.b_placeholder.Text = "placeholder";
+            this.b_placeholder.Visible = false;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.ClientSize = new System.Drawing.Size(665, 601);
+            this.Controls.Add(this.b_placeholder);
+            this.Font = new System.Drawing.Font("Bahnschrift Light", 10F);
+            this.ForeColor = System.Drawing.Color.LightGray;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "mainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Layout";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label b_placeholder;
+    }
+}
+
---
+++ mainForm.cs
@@ -0,0 +1,105 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LayoutCustomization
+{
+    public partial class mainForm : Form
+    {
+        public string currentDir = Environment.CurrentDirectory;
+        public string layoutConfig;
+
+        public mainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+            layoutConfig = Path.Combine(currentDir, "layout.json");
+            if (File.Exists(layoutConfig))
+            {
+                drawLayout();
+            }
+            else
+            {
+                this.Text = "Layout.json doesn\'t exist";
+            }
+
+            b_placeholder.Select();
+        }
+
+        public void drawLayout()
+        {
+            string layout_content = File.ReadAllText(layoutConfig);
+            JObject _layout = JObject.Parse(layout_content);
+            JArray _tabs = (JArray)_layout["Tabs"];
+
+            for (int i = 0; i < _tabs.Count; i++)
+            {
+                JObject property = (JObject)_tabs[i];
+                Debug.WriteLine(property);
+
+                string property_text = property["Text"].ToString();
+                string property_name = property["Name"].ToString();
+
+                Button btn = new Button();
+                btn.Name = property_name;
+                btn.Text = property_text;
+                btn.Size = new Size(180, 35);
+                btn.Location = new Point(15, 25 + (i * 40));
+                btn.Visible = true;
+                btn.MouseDown += new MouseEventHandler(btn_MouseDown);
+                btn.FlatAppearance.BorderSize = 1;
+                btn.FlatAppearance.BorderColor = Color.FromArgb(100, 100, 100);
+                btn.FlatStyle= FlatStyle.Flat;
+                btn.Cursor = Cursors.Hand;
+
+                int spacing = 10;
+                int panelWidth = this.ClientSize.Width - btn.Size.Width - spacing - 20;
+                int panelHeight = this.ClientSize.Height - 30;
+
+                Panel _region = new Panel();
+                _region.Name = $"{property_name}_region";
+                _region.Size = new Size(panelWidth, panelHeight);
+                _region.BorderStyle = BorderStyle.FixedSingle;
+                _region.Location = new Point(btn.Location.X + btn.Size.Width + spacing, 25);
+                _region.Anchor = AnchorStyles.Left | AnchorStyles.Top | AnchorStyles.Right | AnchorStyles.Bottom;
+
+                this.Controls.Add(btn);
+                this.Controls.Add(_region);
+            }   
+        }
+
+        private void btn_MouseDown(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Button btn = (System.Windows.Forms.Button)sender;
+
+            if (btn.Text != "")
+            {
+                foreach (Control otherButton in this.Controls)
+                {
+                    if (otherButton is Button otherBtn)
+                    {
+                        otherBtn.ForeColor = Color.LightGray;
+                        otherBtn.FlatAppearance.BorderColor = Color.FromArgb(100, 100, 100);
+                    }
+                }
+                btn.ForeColor = Color.DodgerBlue;
+                btn.FlatAppearance.BorderColor = Color.DodgerBlue;
+            }
+
+            b_placeholder.Select();
+        }
+
+    }
+}
